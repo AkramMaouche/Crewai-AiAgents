63bfdbd2cb02776fc2c28e0330fb7218544dee03
Akram <akram.maouche07@gmail.com>
2025-10-01 22:42:00 +0100

    create task with my baby girl

---
+++ task.py
@@ -0,0 +1,21 @@
+from crewai import Crew, Task
+from agents import blog_reseacher, blog_writer 
+from tools import yt_Tool 
+
+
+researcher_task = Task(
+    agent= blog_reseacher, 
+    description= ('identify the video {topic}'
+    "get detailed information about the video from the youtube channels" ),
+    expected_output="A comprehensive 3 paragraph long report base on the {topic} of video content  ",
+    tools= [yt_Tool]
+    )
+
+write_task = Task(
+    agent= blog_writer, 
+    description= ("get the information from the youtube channels on the topic {topic}" ),
+    expected_output=" Summarize the info from the yutube channel video on the topic {topic}",
+    tools= [yt_Tool],
+    async_execution= False  
+    output_file= 'new-blog-post.md '
+    )
